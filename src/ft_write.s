b928eca3bb7986db036d8f1034cb086cc6fa896a
alesconese <alesdetoledo@gmail.com>
2026-09-06 20:26:15 +0200

    add: ft_write

---
+++ src/ft_write.s
@@ -0,0 +1,30 @@
+global ft_write
+
+; =======================================
+; ssize_t	ft_write(int fd, const void *buf, size_t count);
+;
+; fd		->	rdi
+; *buf		->	rsi
+; count		->	rdx
+; return	->	rax
+; =======================================
+
+extern __errno_location
+
+section .text
+
+ft_write:
+	mov		rax, 1					; Load the syscall number for write (1) into rax
+	syscall							; Execute the syscall. Arguments are passed in the same registers we received them (rdi, rsi, rdx)
+	test	rax, rax				; Perform bitwise AND on the return value to set Sign Flag. SF = 1 if negative (error)
+	js		.error					; Jump to error handling if SF = 1 (error returned). Error code stored in rax
+	ret								; If no error, return the number of bytes written (in rax)
+
+.error:
+	neg		rax							; Negate rax to get positive errno code
+	push	rax							; Push errno code to stack temporarily
+	call	__errno_location wrt ..plt	; Call __errno_location to save its address in rax. Procedure Linkage Table needed for Position Independent Executable
+	pop		rcx							; Pop errno code from stack into rcx
+	mov		[rax], rcx					; Store errno code at the address returned by __errno_location
+	mov		rax, -1						; Set return value to -1 to indicate error
+	ret
--- test/main.c
+++ test/main.c
@@ -2,14 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 int main(void)
 {
 	/********** Test of ft_strlen **********/
-	printf("*** TESTING FUNCTION: ft_strlen ***\n");
+	printf("***** TESTING FUNCTION: ft_strlen** ***\n");
 	{
+		printf("\n* TEST 1 - Non-empty string\n");
+
 		const char *test_str = "Hello, World!";
-		printf("\n  TEST 1\n  <Input: \"%s\"\n", test_str);
+		printf("  <Input: \"%s\"\n", test_str);
 
 		size_t result1 = strlen(test_str);
 		printf("  >Output with strlen: %zu\n", result1);
@@ -18,56 +21,95 @@ int main(void)
 		printf("  >Output with ft_strlen: %zu\n", result2);
 
 		if (result1 == result2)
-			printf("  RESULT: ✅ PASS\n");
+			printf("* RESULT: ✅ PASS\n");
 		else
-			printf("  RESULT: ❌ FAIL\n");
+			printf("* RESULT: ❌ FAIL\n");
+	}
+	{
+		printf("\n* TEST 2 - Empty string\n");
+
+		const char *test_str = "";
+		printf("  <Input: \"%s\"\n", test_str);
+
+		size_t result1 = strlen(test_str);
+		printf("  >Output with strlen: %zu\n", result1);
+
+		size_t result2 = ft_strlen(test_str);
+		printf("  >Output with ft_strlen: %zu\n", result2);
+
+		if (result1 == result2)
+			printf("* RESULT: ✅ PASS\n");
+		else
+			printf("* RESULT: ❌ FAIL\n");
 	}
 
 	/********** Test of ft_strcpy **********/
-	printf("\n*** TESTING FUNCTION: ft_strcpy ***\n");
+	printf("\n***** TESTING FUNCTION: ft_strcpy** ***\n");
 	{
-		const char *src = "Copy this string!";
-		printf("\n  TEST 1\n  <Source: \"%s\"\n", src);
-		
-		char dest1[50]; // Ensure the destination buffer is large enough
-		
-		strcpy(dest1, src);
-		printf("  >Destination with strcpy: \"%s\"\n", dest1);
-		
-		char dest2[50]; // Ensure the destination buffer is large enough
+		printf("\n* TEST 1 - Non-empty string\n");
 
-		ft_strcpy(dest2, src);
-		printf("  >Destination with ft_strcpy: \"%s\"\n", dest2);
+		const char *src = "Copy this string!";
+		printf("  <Source: \"%s\"\n", src);
+		
+		char dest1[50];
+		char *dest1_ptr = strcpy(dest1, src);
+		printf("  >Destination with strcpy: \"%s\"\n", dest1_ptr);
+		
+		char dest2[50];
+		char *dest2_ptr = ft_strcpy(dest2, src);
+		printf("  >Destination with ft_strcpy: \"%s\"\n", dest2_ptr);
 
 		if (strcmp(dest1, dest2) == 0)
-			printf("  RESULT: ✅ PASS\n");
+			printf("* RESULT: ✅ PASS\n");
 		else
-			printf("  RESULT: ❌ FAIL\n");
+			printf("* RESULT: ❌ FAIL\n");
 	}
 	{
+		printf("\n* TEST 2 - Empty string\n");
+
 		const char *src = "";
-		printf("\n  TEST 2\n  <Source: \"%s\"\n", src);
+		printf("  <Source: \"%s\"\n", src);
 
-		char dest1[50]; // Ensure the destination buffer is large enough
-		strcpy(dest1, src);
-		printf("  >Destination with strcpy: \"%s\"\n", dest1);
+		char dest1[50];
+		char *dest1_ptr = strcpy(dest1, src);
+		printf("  >Destination with strcpy: \"%s\"\n", dest1_ptr);
 
-		char dest2[50]; // Ensure the destination buffer is large enough
-		ft_strcpy(dest2, src);
-		printf("  >Destination with ft_strcpy: \"%s\"\n", dest2);
+		char dest2[50];
+		char *dest2_ptr = ft_strcpy(dest2, src);
+		printf("  >Destination with ft_strcpy: \"%s\"\n", dest2_ptr);
 
 		if (strcmp(dest1, dest2) == 0)
-			printf("  RESULT: ✅ PASS\n");
+			printf("* RESULT: ✅ PASS\n");
 		else
-			printf("  RESULT: ❌ FAIL\n");
+			printf("* RESULT: ❌ FAIL\n");
 	}
 	
 	/********** Test of ft_strcmp **********/
-	printf("\n*** TESTING FUNCTION: ft_strcmp ***\n");
+	printf("\n***** TESTING FUNCTION: ft_strcmp** ***\n");
 	{
-		const char *s1 = "Hello";
+		printf("\n* TEST 1 - Identical strings\n");
+
+		const char *s1 = "Hello World!";
+		const char *s2 = "Hello World!";
+		printf("  <String 1: \"%s\"\n  <String 2: \"%s\"\n", s1, s2);
+
+		int result1 = strcmp(s1, s2);
+		printf("  >Output with strcmp: %d\n", result1);
+
+		int result2 = ft_strcmp(s1, s2);
+		printf("  >Output with ft_strcmp: %d\n", result2);
+
+		if (result1 == result2)
+			printf("* RESULT: ✅ PASS\n");
+		else
+			printf("* RESULT: ❌ FAIL\n");
+	}
+	{
+		printf("\n* TEST 2 - S1 longer than S2\n");
+
+		const char *s1 = "Hello World!";
 		const char *s2 = "Hello";
-		printf("\n  TEST 1\n  <String 1: \"%s\"\n  <String 2: \"%s\"\n", s1, s2);
+		printf("  <String 1: \"%s\"\n  <String 2: \"%s\"\n", s1, s2);
 
 		int result1 = strcmp(s1, s2);
 		printf("  >Output with strcmp: %d\n", result1);
@@ -76,14 +118,16 @@ int main(void)
 		printf("  >Output with ft_strcmp: %d\n", result2);
 
 		if (result1 == result2)
-			printf("  RESULT: ✅ PASS\n");
+			printf("* RESULT: ✅ PASS\n");
 		else
-			printf("  RESULT: ❌ FAIL\n");
+			printf("* RESULT: ❌ FAIL\n");
 	}
 	{
-		const char *s1 = "Hello1";
-		const char *s2 = "Hello";
-		printf("\n  TEST 2\n  <String 1: \"%s\"\n  <String 2: \"%s\"\n", s1, s2);
+		printf("\n* TEST 3 - Different character found\n");
+
+		const char *s1 = "Hello World!";
+		const char *s2 = "Hello Wxrld!";
+		printf("\n* TEST 3\n  <String 1: \"%s\"\n  <String 2: \"%s\"\n", s1, s2);
 
 		int result1 = strcmp(s1, s2);
 		printf("  >Output with strcmp: %d\n", result1);
@@ -92,27 +136,72 @@ int main(void)
 		printf("  >Output with ft_strcmp: %d\n", result2);
 
 		if (result1 == result2)
-			printf("  RESULT: ✅ PASS\n");
+			printf("* RESULT: ✅ PASS\n");
 		else
-			printf("  RESULT: ❌ FAIL\n");
+			printf("* RESULT: ❌ FAIL\n");
+	}
+
+	/********** Test of ft_write **********/
+	printf("\n***** TESTING FUNCTION: ft_write *****\n");
+	{
+		printf("\n* TEST 1 - Valid string to stdout\n");
+
+		const char *str = "Hello, World!";
+		printf("  <String: \"%s\"\n", str);
+
+		ssize_t result1 = write(1, str, strlen(str));
+		printf("\n  >Output with write: %zd\n", result1);
+
+		ssize_t result2 = ft_write(1, str, strlen(str));
+		printf("\n  >Output with ft_write: %zd\n", result2);
+
+		if (result1 == result2)
+			printf("* RESULT: ✅ PASS\n");
+		else
+			printf("* RESULT: ❌ FAIL\n");
 	}
 	{
-		const char *s1 = "Hello1";
-		const char *s2 = "Hello5";
-		printf("\n  TEST 3\n  <String 1: \"%s\"\n  <String 2: \"%s\"\n", s1, s2);
+		printf("\n* TEST 2 - NULL string to stdout\n");
 
-		int result1 = strcmp(s1, s2);
-		printf("  >Output with strcmp: %d\n", result1);
+		const char *str = NULL;
+		printf("  <String: \"%s\"\n", str);
 
-		int result2 = ft_strcmp(s1, s2);
-		printf("  >Output with ft_strcmp: %d\n", result2);
+		errno = 0;
+		ssize_t result1 = write(1, str, 42);
+		printf("  >Output with write: %zd\n", result1);
+		printf("  >Error with write: %s [errno: %d]\n", strerror(errno), errno);
+
+		errno = 0;
+		ssize_t result2 = ft_write(1, str, 42);
+		printf("  >Output with ft_write: %zd\n", result2);
+		printf("  >Error with ft_write: %s [errno: %d]\n", strerror(errno), errno);
 
 		if (result1 == result2)
-			printf("  RESULT: ✅ PASS\n");
+			printf("* RESULT: ✅ PASS\n");
 		else
-			printf("  RESULT: ❌ FAIL\n");
+			printf("* RESULT: ❌ FAIL\n");
 	}
+	{
+		printf("\n* TEST 3 - Invalid file descriptor\n");
 
+		const char *str = "Hello, World!";
+		printf("  <String: \"%s\"\n", str);
+
+		errno = 0;
+		ssize_t result1 = write(-1, str, strlen(str));
+		printf("  >Output with write: %zd\n", result1);
+		printf("  >Error with write: %s [errno: %d]\n", strerror(errno), errno);
+
+		errno = 0;
+		ssize_t result2 = ft_write(-1, str, strlen(str));
+		printf("  >Output with ft_write: %zd\n", result2);
+		printf("  >Error with ft_write: %s [errno: %d]\n", strerror(errno), errno);
+
+		if (result1 == result2)
+			printf("* RESULT: ✅ PASS\n");
+		else
+			printf("* RESULT: ❌ FAIL\n");
+	}
 
     return (0);
 }
